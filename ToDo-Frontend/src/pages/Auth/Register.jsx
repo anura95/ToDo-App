f33cfe9f08e9f1fbcc3c7100299fbcf59928a12a
anura95 <117428439+anura95@users.noreply.github.com>
2024-11-17 21:56:31 +0530

    Update Register.jsx

--- ToDo-Frontend/src/pages/Auth/Register.jsx
+++ ToDo-Frontend/src/pages/Auth/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:5000/register', {
+      const response = await fetch('https://todo-app-backend-l829.onrender.com/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
