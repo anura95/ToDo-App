d21ba22aef83c5fbb505981f8059fd4f509f3628
anura95 <117428439+anura95@users.noreply.github.com>
2024-11-17 21:56:01 +0530

    Update Login.jsx

--- ToDo-Frontend/src/pages/Auth/Login.jsx
+++ ToDo-Frontend/src/pages/Auth/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:5000/login', {
+      const response = await fetch('https://todo-app-backend-l829.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
